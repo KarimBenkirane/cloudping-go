ecd4fc2d0a08d5cf35e36ecd70fe646481954a00
Karim Benkirane <benkiranemedkarim@gmail.com>
2026-03-27 09:28:32 +0100

    Add pingRegion method with a keep-alive request

--- pinger/pinger.go
+++ pinger/pinger.go
@@ -1,23 +1,45 @@
 package pinger
 
 import (
+	"io"
 	"net/http"
 	"time"
 )
 
-func getCurrentTime() time.Time {
-	return time.Now()
-}
+var client http.Client = http.Client{Timeout: 3 * time.Second}
 
 func pingRegion(region Region) Result {
-	startTime := getCurrentTime()
-	_, err := http.Head(region.Url)
-	endTime := time.Since(startTime).Milliseconds()
-	if err != nil {
-		return Result{Region: region, Latency: endTime, Status: err.Error()}
-	}
-	return Result{Region: region, Latency: endTime, Status: "success"}
 
+	// Warmup to avoid DNS & TLS time in the next call
+	err := helper(region.Url)
+	if err != nil {
+		return Result{Region: region, Latency: 0, Status: err.Error()}
+	}
+
+	// Calculate the latency
+	startTime := time.Now()
+	err = helper(region.Url)
+	if err != nil {
+		return Result{Region: region, Latency: 0, Status: err.Error()}
+	}
+	endTime := time.Since(startTime)
+
+	return Result{Region: region, Latency: endTime.Milliseconds(), Status: "success"}
+
+}
+
+func helper(url string) error {
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	io.Copy(io.Discard, res.Body) // Read the body until EOF to "re-use a persistent TCP connection to the server for a subsequent "keep-alive" request."
+	return nil
 }
 
 func Ping(regions Regions) []Result {
