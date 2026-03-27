b386080d2f58b82d7b451282bc34701d6bfc58d6
Karim Benkirane <benkiranemedkarim@gmail.com>
2026-03-27 11:56:15 +0100

    Move regions.json to pinger & add copyright information in main.go

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 /*
-Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2026 Mohamed Karim Benkirane <benkiranemedkarim@gmail.com>
 */
 package main
 
-import "github.com/KarimBenkirane/cloudping-go/cmd"
+import (
+	"github.com/KarimBenkirane/cloudping-go/cmd"
+)
 
 func main() {
 	cmd.Execute()
